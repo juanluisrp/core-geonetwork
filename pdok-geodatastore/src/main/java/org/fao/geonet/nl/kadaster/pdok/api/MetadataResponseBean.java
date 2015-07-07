ff2c76aa235ac67bad7c7e52f3b7e5a931fcf6c6
Jeroen Bloemscheer <jeroen.bloemscheer@cgi.com>
2015-07-07 14:20:07 +0200

    Added pdok-geodatstore module

---
+++ pdok-geodatastore/src/main/java/org/fao/geonet/nl/kadaster/pdok/api/MetadataResponseBean.java
@@ -0,0 +1,16 @@
+package org.fao.geonet.nl.kadaster.pdok.api;
+
+/**
+ * Created by JuanLuis on 30/06/2015.
+ */
+public class MetadataResponseBean {
+    private String identifier;
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+}
