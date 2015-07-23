224aa6294691f8d8b73265ca19b51eeeff033cd1
paul van genuchten <paul.vangenuchten@geocat.net>
2015-07-23 13:12:23 +0200

    if an upload error occurs, create a dummy-object to contain the error message

--- web-ui/src/main/resources/catalog/views/geodatastore/module.js
+++ web-ui/src/main/resources/catalog/views/geodatastore/module.js
@@ -118,6 +118,7 @@
 	  $scope.formModified = false;
 
 		$scope.setMD = function(md) {
+			console.log(md);
 			$scope.mdSelected = angular.copy(md);
 			$scope.hasSelected = true;
       if (md.topicCategories && md.topicCategories.length > 0) {
